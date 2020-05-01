128f73ec54d7c58c2dc8d2fbd5d897c0497c6678
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-05-01 22:17:01 +0400

    add av icons

---
+++ src/icons/av/index.js
@@ -0,0 +1,170 @@
+import add_to_queue from './ic_add_to_queue_48px.svg'
+import airplay from './ic_airplay_48px.svg'
+import album from './ic_album_48px.svg'
+import art_track from './ic_art_track_48px.svg'
+import av_timer from './ic_av_timer_48px.svg'
+import branding_watermark from './ic_branding_watermark_48px.svg'
+import call_to_action from './ic_call_to_action_48px.svg'
+import closed_caption from './ic_closed_caption_48px.svg'
+import equalizer from './ic_equalizer_48px.svg'
+import explicit from './ic_explicit_48px.svg'
+import fast_forward from './ic_fast_forward_48px.svg'
+import fast_rewind from './ic_fast_rewind_48px.svg'
+import featured_play_list from './ic_featured_play_list_48px.svg'
+import featured_video from './ic_featured_video_48px.svg'
+import fiber_dvr from './ic_fiber_dvr_48px.svg'
+import fiber_manual_record from './ic_fiber_manual_record_48px.svg'
+import fiber_new from './ic_fiber_new_48px.svg'
+import fiber_pin from './ic_fiber_pin_48px.svg'
+import fiber_smart_record from './ic_fiber_smart_record_48px.svg'
+import forward_5 from './ic_forward_5_48px.svg'
+import forward_10 from './ic_forward_10_48px.svg'
+import forward_30 from './ic_forward_30_48px.svg'
+import games from './ic_games_48px.svg'
+import hd from './ic_hd_24px.svg'
+import hearing from './ic_hearing_48px.svg'
+import high_quality from './ic_high_quality_48px.svg'
+import library_add from './ic_library_add_48px.svg'
+import library_books from './ic_library_books_48px.svg'
+import library_music from './ic_library_music_48px.svg'
+import loop from './ic_loop_48px.svg'
+import mic from './ic_mic_48px.svg'
+import mic_none from './ic_mic_none_48px.svg'
+import mic_off from './ic_mic_off_48px.svg'
+import movie from './ic_movie_48px.svg'
+import music_video from './ic_music_video_48px.svg'
+import new_releases from './ic_new_releases_48px.svg'
+import not_interested from './ic_not_interested_48px.svg'
+import note from './ic_note_48px.svg'
+import pause from './ic_pause_48px.svg'
+import pause_circle_filled from './ic_pause_circle_filled_48px.svg'
+import pause_circle_outline from './ic_pause_circle_outline_48px.svg'
+import play_arrow from './ic_play_arrow_48px.svg'
+import play_circle_filled from './ic_play_circle_filled_48px.svg'
+import play_circle_filled_white from './ic_play_circle_filled_white_48px.svg'
+import play_circle_outline from './ic_play_circle_outline_48px.svg'
+import playlist_add from './ic_playlist_add_48px.svg'
+import playlist_add_check from './ic_playlist_add_check_48px.svg'
+import playlist_play from './ic_playlist_play_48px.svg'
+import queue from './ic_queue_48px.svg'
+import queue_music from './ic_queue_music_48px.svg'
+import queue_play_next from './ic_queue_play_next_48px.svg'
+import radio from './ic_radio_48px.svg'
+import recent_actors from './ic_recent_actors_48px.svg'
+import remove_from_queue from './ic_remove_from_queue_48px.svg'
+import repeat from './ic_repeat_48px.svg'
+import repeat_one from './ic_repeat_one_48px.svg'
+import replay_5 from './ic_replay_5_48px.svg'
+import replay_10 from './ic_replay_10_48px.svg'
+import replay_30 from './ic_replay_30_48px.svg'
+import replay from './ic_replay_48px.svg'
+import shuffle from './ic_shuffle_48px.svg'
+import skip_next from './ic_skip_next_48px.svg'
+import skip_previous from './ic_skip_previous_48px.svg'
+import slow_motion_video from './ic_slow_motion_video_48px.svg'
+import snooze from './ic_snooze_48px.svg'
+import sort_by_alpha from './ic_sort_by_alpha_24px.svg'
+import stop from './ic_stop_48px.svg'
+import subscriptions from './ic_subscriptions_48px.svg'
+import subtitles from './ic_subtitles_48px.svg'
+import surround_sound from './ic_surround_sound_48px.svg'
+import video_call from './ic_video_call_48px.svg'
+import video_label from './ic_video_label_48px.svg'
+import video_library from './ic_video_library_48px.svg'
+import videocam from './ic_videocam_48px.svg'
+import videocam_off from './ic_videocam_off_48px.svg'
+import volume_down from './ic_volume_down_48px.svg'
+import volume_mute from './ic_volume_mute_48px.svg'
+import volume_off from './ic_volume_off_48px.svg'
+import volume_up from './ic_volume_up_48px.svg'
+import web from './ic_web_48px.svg'
+import web_asset from './ic_web_asset_48px.svg'
+
+const icons = {
+    add_to_queue,
+    airplay,
+    album,
+    art_track,
+    av_timer,
+    branding_watermark,
+    call_to_action,
+    closed_caption,
+    equalizer,
+    explicit,
+    fast_forward,
+    fast_rewind,
+    featured_play_list,
+    featured_video,
+    fiber_dvr,
+    fiber_manual_record,
+    fiber_new,
+    fiber_pin,
+    fiber_smart_record,
+    forward_5,
+    forward_10,
+    forward_30,
+    games,
+    hd,
+    hearing,
+    high_quality,
+    library_add,
+    library_books,
+    library_music,
+    loop,
+    mic,
+    mic_none,
+    mic_off,
+    movie,
+    music_video,
+    new_releases,
+    not_interested,
+    note,
+    pause,
+    pause_circle_filled,
+    pause_circle_outline,
+    play_arrow,
+    play_circle_filled,
+    play_circle_filled_white,
+    play_circle_outline,
+    playlist_add,
+    playlist_add_check,
+    playlist_play,
+    queue,
+    queue_music,
+    queue_play_next,
+    radio,
+    recent_actors,
+    remove_from_queue,
+    repeat,
+    repeat_one,
+    replay_5,
+    replay_10,
+    replay_30,
+    replay,
+    shuffle,
+    skip_next,
+    skip_previous,
+    slow_motion_video,
+    snooze,
+    sort_by_alpha,
+    stop,
+    subscriptions,
+    subtitles,
+    surround_sound,
+    video_call,
+    video_label,
+    video_library,
+    videocam,
+    videocam_off,
+    volume_down,
+    volume_mute,
+    volume_off,
+    volume_up,
+    web,
+    web_asset,
+}
+
+export default {
+    icons,
+    names: Object.keys(icons)
+}
