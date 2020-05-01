61807b639d0e9fb3384871b8748da4f013a406ec
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-05-01 22:18:26 +0400

    add image icons

---
+++ src/icons/image/index.js
@@ -0,0 +1,318 @@
+import rotate_right from './ic_rotate_right_48px.svg'
+import style from './ic_style_48px.svg'
+import crop_free from './ic_crop_free_48px.svg'
+import timelapse from './ic_timelapse_48px.svg'
+import music_note from './ic_music_note_48px.svg'
+import flash_auto from './ic_flash_auto_48px.svg'
+import remove_red_eye from './ic_remove_red_eye_48px.svg'
+import brightness_7 from './ic_brightness_7_48px.svg'
+import tune from './ic_tune_48px.svg'
+import leak_add from './ic_leak_add_48px.svg'
+import filter_b_and_w from './ic_filter_b_and_w_48px.svg'
+import crop_landscape from './ic_crop_landscape_48px.svg'
+import details from './ic_details_48px.svg'
+import looks_6 from './ic_looks_6_48px.svg'
+import broken_image from './ic_broken_image_48px.svg'
+import panorama_wide_angle from './ic_panorama_wide_angle_48px.svg'
+import movie_creation from './ic_movie_creation_48px.svg'
+import camera_front from './ic_camera_front_48px.svg'
+import view_compact from './ic_view_compact_48px.svg'
+import brightness_1 from './ic_brightness_1_48px.svg'
+import timer from './ic_timer_48px.svg'
+import slideshow from './ic_slideshow_48px.svg'
+import add_to_photos from './ic_add_to_photos_48px.svg'
+import filter_frames from './ic_filter_frames_48px.svg'
+import nature_people from './ic_nature_people_48px.svg'
+import switch_camera from './ic_switch_camera_48px.svg'
+import flare from './ic_flare_48px.svg'
+import linked_camera from './ic_linked_camera_48px.svg'
+import filter_hdr from './ic_filter_hdr_48px.svg'
+import panorama_horizontal from './ic_panorama_horizontal_48px.svg'
+import photo_size_select_large from './ic_photo_size_select_large_24px.svg'
+import collections from './ic_collections_48px.svg'
+import texture from './ic_texture_48px.svg'
+import crop_5_4 from './ic_crop_5_4_48px.svg'
+import timer_3 from './ic_timer_3_48px.svg'
+import grid_on from './ic_grid_on_48px.svg'
+import flash_on from './ic_flash_on_48px.svg'
+import blur_linear from './ic_blur_linear_48px.svg'
+import color_lens from './ic_color_lens_48px.svg'
+import photo_camera from './ic_photo_camera_48px.svg'
+import filter_vintage from './ic_filter_vintage_48px.svg'
+import panorama from './ic_panorama_48px.svg'
+import center_focus_weak from './ic_center_focus_weak_48px.svg'
+import image_aspect_ratio from './ic_image_aspect_ratio_48px.svg'
+import photo from './ic_photo_48px.svg'
+import burst_mode from './ic_burst_mode_48px.svg'
+import leak_remove from './ic_leak_remove_48px.svg'
+import filter_2 from './ic_filter_2_48px.svg'
+import filter_6 from './ic_filter_6_48px.svg'
+import brightness_3 from './ic_brightness_3_48px.svg'
+import brightness_2 from './ic_brightness_2_48px.svg'
+import filter_center_focus from './ic_filter_center_focus_48px.svg'
+import image from './ic_image_48px.svg'
+import crop_16_9 from './ic_crop_16_9_48px.svg'
+import lens from './ic_lens_48px.svg'
+import loupe from './ic_loupe_48px.svg'
+import add_a_photo from './ic_add_a_photo_48px.svg'
+import looks from './ic_looks_48px.svg'
+import exposure_neg_2 from './ic_exposure_neg_2_48px.svg'
+import tonality from './ic_tonality_48px.svg'
+import filter_none from './ic_filter_none_48px.svg'
+import crop_original from './ic_crop_original_48px.svg'
+import compare from './ic_compare_48px.svg'
+import crop_portrait from './ic_crop_portrait_48px.svg'
+import tag_faces from './ic_tag_faces_48px.svg'
+import crop_din from './ic_crop_din_48px.svg'
+import camera_roll from './ic_camera_roll_48px.svg'
+import timer_off from './ic_timer_off_48px.svg'
+import filter_tilt_shift from './ic_filter_tilt_shift_48px.svg'
+import crop_square from './ic_crop_square_48px.svg'
+import dehaze from './ic_dehaze_48px.svg'
+import hdr_on from './ic_hdr_on_48px.svg'
+import transform from './ic_transform_48px.svg'
+import photo_filter from './ic_photo_filter_48px.svg'
+import filter_9 from './ic_filter_9_48px.svg'
+import filter_9_plus from './ic_filter_9_plus_48px.svg'
+import straighten from './ic_straighten_48px.svg'
+import crop_rotate from './ic_crop_rotate_48px.svg'
+import filter_5 from './ic_filter_5_48px.svg'
+import camera_rear from './ic_camera_rear_48px.svg'
+import edit from './ic_edit_48px.svg'
+import blur_off from './ic_blur_off_48px.svg'
+import brightness_4 from './ic_brightness_4_48px.svg'
+import rotate_left from './ic_rotate_left_48px.svg'
+import photo_size_select_actual from './ic_photo_size_select_actual_24px.svg'
+import navigate_before from './ic_navigate_before_48px.svg'
+import filter_4 from './ic_filter_4_48px.svg'
+import grid_off from './ic_grid_off_48px.svg'
+import center_focus_strong from './ic_center_focus_strong_48px.svg'
+import looks_5 from './ic_looks_5_48px.svg'
+import looks_one from './ic_looks_one_48px.svg'
+import rotate_90_degrees_ccw from './ic_rotate_90_degrees_ccw_48px.svg'
+import gradient from './ic_gradient_48px.svg'
+import colorize from './ic_colorize_48px.svg'
+import brush from './ic_brush_48px.svg'
+import audiotrack from './ic_audiotrack_48px.svg'
+import movie_filter from './ic_movie_filter_48px.svg'
+import adjust from './ic_adjust_48px.svg'
+import collections_bookmark from './ic_collections_bookmark_24px.svg'
+import flip from './ic_flip_48px.svg'
+import nature from './ic_nature_48px.svg'
+import filter_drama from './ic_filter_drama_48px.svg'
+import control_point_duplicate from './ic_control_point_duplicate_48px.svg'
+import exposure_plus_2 from './ic_exposure_plus_2_48px.svg'
+import picture_as_pdf from './ic_picture_as_pdf_48px.svg'
+import assistant_photo from './ic_assistant_photo_48px.svg'
+import healing from './ic_healing_48px.svg'
+import filter_7 from './ic_filter_7_48px.svg'
+import monochrome_photos from './ic_monochrome_photos_48px.svg'
+import exposure_neg_1 from './ic_exposure_neg_1_48px.svg'
+import filter from './ic_filter_48px.svg'
+import looks_4 from './ic_looks_4_48px.svg'
+import photo_album from './ic_photo_album_48px.svg'
+import view_comfy from './ic_view_comfy_48px.svg'
+import hdr_weak from './ic_hdr_weak_48px.svg'
+import filter_1 from './ic_filter_1_48px.svg'
+import brightness_6 from './ic_brightness_6_48px.svg'
+import iso from './ic_iso_48px.svg'
+import looks_two from './ic_looks_two_48px.svg'
+import brightness_5 from './ic_brightness_5_48px.svg'
+import hdr_strong from './ic_hdr_strong_48px.svg'
+import crop_7_5 from './ic_crop_7_5_48px.svg'
+import switch_video from './ic_switch_video_48px.svg'
+import assistant from './ic_assistant_48px.svg'
+import panorama_fish_eye from './ic_panorama_fish_eye_48px.svg'
+import photo_size_select_small from './ic_photo_size_select_small_24px.svg'
+import filter_3 from './ic_filter_3_48px.svg'
+import crop_3_2 from './ic_crop_3_2_48px.svg'
+import photo_library from './ic_photo_library_48px.svg'
+import camera_alt from './ic_camera_alt_48px.svg'
+import flash_off from './ic_flash_off_48px.svg'
+import timer_10 from './ic_timer_10_48px.svg'
+import blur_circular from './ic_blur_circular_48px.svg'
+import portrait from './ic_portrait_48px.svg'
+import navigate_next from './ic_navigate_next_48px.svg'
+import exposure from './ic_exposure_48px.svg'
+import camera from './ic_camera_48px.svg'
+import filter_8 from './ic_filter_8_48px.svg'
+import blur_on from './ic_blur_on_48px.svg'
+import hdr_off from './ic_hdr_off_48px.svg'
+import crop from './ic_crop_48px.svg'
+import grain from './ic_grain_48px.svg'
+import exposure_zero from './ic_exposure_zero_48px.svg'
+import panorama_vertical from './ic_panorama_vertical_48px.svg'
+import looks_3 from './ic_looks_3_48px.svg'
+import palette from './ic_palette_48px.svg'
+import control_point from './ic_control_point_48px.svg'
+import landscape from './ic_landscape_48px.svg'
+import exposure_plus_1 from './ic_exposure_plus_1_48px.svg'
+import vignette from './ic_vignette_24px.svg'
+import wb_auto from './ic_wb_auto_48px.svg'
+import wb_cloudy from './ic_wb_cloudy_48px.svg'
+import wb_incandescent from './ic_wb_incandescent_48px.svg'
+import wb_iridescent from './ic_wb_iridescent_48px.svg'
+import wb_sunny from './ic_wb_sunny_48px.svg'
+
+const icons = {
+    rotate_right,
+    style,
+    crop_free,
+    timelapse,
+    music_note,
+    flash_auto,
+    remove_red_eye,
+    brightness_7,
+    tune,
+    leak_add,
+    filter_b_and_w,
+    crop_landscape,
+    details,
+    looks_6,
+    broken_image,
+    panorama_wide_angle,
+    movie_creation,
+    camera_front,
+    view_compact,
+    brightness_1,
+    timer,
+    slideshow,
+    add_to_photos,
+    filter_frames,
+    nature_people,
+    switch_camera,
+    flare,
+    linked_camera,
+    filter_hdr,
+    panorama_horizontal,
+    photo_size_select_large,
+    collections,
+    texture,
+    crop_5_4,
+    timer_3,
+    grid_on,
+    flash_on,
+    blur_linear,
+    color_lens,
+    photo_camera,
+    filter_vintage,
+    panorama,
+    center_focus_weak,
+    image_aspect_ratio,
+    photo,
+    burst_mode,
+    leak_remove,
+    filter_2,
+    filter_6,
+    brightness_3,
+    brightness_2,
+    filter_center_focus,
+    image,
+    crop_16_9,
+    lens,
+    loupe,
+    add_a_photo,
+    looks,
+    exposure_neg_2,
+    tonality,
+    filter_none,
+    crop_original,
+    compare,
+    crop_portrait,
+    tag_faces,
+    crop_din,
+    camera_roll,
+    timer_off,
+    filter_tilt_shift,
+    crop_square,
+    dehaze,
+    hdr_on,
+    transform,
+    photo_filter,
+    filter_9,
+    filter_9_plus,
+    straighten,
+    crop_rotate,
+    filter_5,
+    camera_rear,
+    edit,
+    blur_off,
+    brightness_4,
+    rotate_left,
+    photo_size_select_actual,
+    navigate_before,
+    filter_4,
+    grid_off,
+    center_focus_strong,
+    looks_5,
+    looks_one,
+    rotate_90_degrees_ccw,
+    gradient,
+    colorize,
+    brush,
+    audiotrack,
+    movie_filter,
+    adjust,
+    collections_bookmark,
+    flip,
+    nature,
+    filter_drama,
+    control_point_duplicate,
+    exposure_plus_2,
+    picture_as_pdf,
+    assistant_photo,
+    healing,
+    filter_7,
+    monochrome_photos,
+    exposure_neg_1,
+    filter,
+    looks_4,
+    photo_album,
+    view_comfy,
+    hdr_weak,
+    filter_1,
+    brightness_6,
+    iso,
+    looks_two,
+    brightness_5,
+    hdr_strong,
+    crop_7_5,
+    switch_video,
+    assistant,
+    panorama_fish_eye,
+    photo_size_select_small,
+    filter_3,
+    crop_3_2,
+    photo_library,
+    camera_alt,
+    flash_off,
+    timer_10,
+    blur_circular,
+    portrait,
+    navigate_next,
+    exposure,
+    camera,
+    filter_8,
+    blur_on,
+    hdr_off,
+    crop,
+    grain,
+    exposure_zero,
+    panorama_vertical,
+    looks_3,
+    palette,
+    control_point,
+    landscape,
+    exposure_plus_1,
+    vignette,
+    wb_auto,
+    wb_cloudy,
+    wb_incandescent,
+    wb_iridescent,
+    wb_sunny
+}
+
+export default {
+    icons,
+    names: Object.keys(icons)
+}
