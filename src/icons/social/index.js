236654037c681c3feaccd0ba0c381a41f838db4a
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-05-01 22:19:43 +0400

    add social icons

---
+++ src/icons/social/index.js
@@ -0,0 +1,68 @@
+import cake from './ic_cake_48px.svg'
+import domain from './ic_domain_48px.svg'
+import group from './ic_group_48px.svg'
+import group_add from './ic_group_add_48px.svg'
+import location_city from './ic_location_city_48px.svg'
+import mood from './ic_mood_48px.svg'
+import mood_bad from './ic_mood_bad_48px.svg'
+import notifications from './ic_notifications_48px.svg'
+import notifications_active from './ic_notifications_active_48px.svg'
+import notifications_none from './ic_notifications_none_48px.svg'
+import notifications_off from './ic_notifications_off_48px.svg'
+import notifications_paused from './ic_notifications_paused_48px.svg'
+import pages from './ic_pages_48px.svg'
+import party_mode from './ic_party_mode_48px.svg'
+import people from './ic_people_48px.svg'
+import people_outline from './ic_people_outline_48px.svg'
+import person from './ic_person_48px.svg'
+import person_add from './ic_person_add_48px.svg'
+import person_outline from './ic_person_outline_48px.svg'
+import plus_one from './ic_plus_one_48px.svg'
+import poll from './ic_poll_48px.svg'
+import _public from './ic_public_48px.svg'
+import school from './ic_school_48px.svg'
+import sentiment_dissatisfied from './ic_sentiment_dissatisfied_48px.svg'
+import sentiment_neutral from './ic_sentiment_neutral_48px.svg'
+import sentiment_satisfied from './ic_sentiment_satisfied_48px.svg'
+import sentiment_very_dissatisfied from './ic_sentiment_very_dissatisfied_48px.svg'
+import sentiment_very_satisfied from './ic_sentiment_very_satisfied_48px.svg'
+import share from './ic_share_48px.svg'
+import whatshot from './ic_whatshot_48px.svg'
+
+const icons = {
+    cake,
+    domain,
+    group,
+    group_add,
+    location_city,
+    mood,
+    mood_bad,
+    notifications,
+    notifications_active,
+    notifications_none,
+    notifications_off,
+    notifications_paused,
+    pages,
+    party_mode,
+    people,
+    people_outline,
+    person,
+    person_add,
+    person_outline,
+    plus_one,
+    poll,
+    _public,
+    school,
+    sentiment_dissatisfied,
+    sentiment_neutral,
+    sentiment_satisfied,
+    sentiment_very_dissatisfied,
+    sentiment_very_satisfied,
+    share,
+    whatshot,
+}
+
+export default {
+    icons,
+    names: Object.keys(icons)
+}
