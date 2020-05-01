08c257f7c081c6d234f6aff25905598f08610bc4
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-05-01 22:18:52 +0400

    add navigation icons

---
+++ src/icons/navigation/index.js
@@ -0,0 +1,62 @@
+import arrow_drop_down_circle from './ic_arrow_drop_down_circle_48px.svg'
+import menu from './ic_menu_48px.svg'
+import close from './ic_close_48px.svg'
+import arrow_back from './ic_arrow_back_48px.svg'
+import subdirectory_arrow_left from './ic_subdirectory_arrow_left_48px.svg'
+import last_page from './ic_last_page_48px.svg'
+import expand_more from './ic_expand_more_48px.svg'
+import arrow_drop_down from './ic_arrow_drop_down_48px.svg'
+import check from './ic_check_48px.svg'
+import arrow_upward from './ic_arrow_upward_48px.svg'
+import fullscreen from './ic_fullscreen_48px.svg'
+import more_vert from './ic_more_vert_48px.svg'
+import arrow_downward from './ic_arrow_downward_48px.svg'
+import arrow_forward from './ic_arrow_forward_48px.svg'
+import cancel from './ic_cancel_48px.svg'
+import chevron_left from './ic_chevron_left_48px.svg'
+import refresh from './ic_refresh_48px.svg'
+import fullscreen_exit from './ic_fullscreen_exit_48px.svg'
+import apps from './ic_apps_48px.svg'
+import expand_less from './ic_expand_less_48px.svg'
+import more_horiz from './ic_more_horiz_48px.svg'
+import chevron_right from './ic_chevron_right_48px.svg'
+import arrow_drop_up from './ic_arrow_drop_up_48px.svg'
+import first_page from './ic_first_page_48px.svg'
+import subdirectory_arrow_right from './ic_subdirectory_arrow_right_48px.svg'
+import unfold_less from './ic_unfold_less_48px.svg'
+import unfold_more from './ic_unfold_more_48px.svg'
+
+const icons = {
+    arrow_drop_down_circle,
+    menu,
+    close,
+    arrow_back,
+    subdirectory_arrow_left,
+    last_page,
+    expand_more,
+    arrow_drop_down,
+    check,
+    arrow_upward,
+    fullscreen,
+    more_vert,
+    arrow_downward,
+    arrow_forward,
+    cancel,
+    chevron_left,
+    refresh,
+    fullscreen_exit,
+    apps,
+    expand_less,
+    more_horiz,
+    chevron_right,
+    arrow_drop_up,
+    first_page,
+    subdirectory_arrow_right,
+    unfold_less,
+    unfold_more
+}
+
+export default {
+    icons,
+    names: Object.keys(icons)
+}
