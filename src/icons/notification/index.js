c4be7a7007ec5af84383e51be54efe6aa3343015
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-05-01 22:19:04 +0400

    add notifications icons

---
+++ src/icons/notification/index.js
@@ -0,0 +1,116 @@
+import folder_special from './ic_folder_special_48px.svg'
+import airline_seat_flat_angled from './ic_airline_seat_flat_angled_48px.svg'
+import airline_seat_legroom_extra from './ic_airline_seat_legroom_extra_48px.svg'
+import do_not_disturb_off from './ic_do_not_disturb_off_48px.svg'
+import airline_seat_recline_extra from './ic_airline_seat_recline_extra_48px.svg'
+import do_not_disturb_alt from './ic_do_not_disturb_alt_48px.svg'
+import airline_seat_flat from './ic_airline_seat_flat_48px.svg'
+import more from './ic_more_48px.svg'
+import enhanced_encryption from './ic_enhanced_encryption_48px.svg'
+import airline_seat_legroom_reduced from './ic_airline_seat_legroom_reduced_48px.svg'
+import confirmation_number from './ic_confirmation_number_48px.svg'
+import do_not_disturb from './ic_do_not_disturb_48px.svg'
+import drive_eta from './ic_drive_eta_48px.svg'
+import event_available from './ic_event_available_48px.svg'
+import bluetooth_audio from './ic_bluetooth_audio_48px.svg'
+import disc_full from './ic_disc_full_48px.svg'
+import airline_seat_recline_normal from './ic_airline_seat_recline_normal_48px.svg'
+import live_tv from './ic_live_tv_48px.svg'
+import event_busy from './ic_event_busy_48px.svg'
+import airline_seat_individual_suite from './ic_airline_seat_individual_suite_48px.svg'
+import adb from './ic_adb_48px.svg'
+import do_not_disturb_on from './ic_do_not_disturb_on_48px.svg'
+import airline_seat_legroom_normal from './ic_airline_seat_legroom_normal_48px.svg'
+import event_note from './ic_event_note_48px.svg'
+import mms from './ic_mms_48px.svg'
+import network_check from './ic_network_check_48px.svg'
+import network_locked from './ic_network_locked_48px.svg'
+import no_encryption from './ic_no_encryption_48px.svg'
+import ondemand_video from './ic_ondemand_video_48px.svg'
+import personal_video from './ic_personal_video_48px.svg'
+import phone_bluetooth_speaker from './ic_phone_bluetooth_speaker_48px.svg'
+import phone_forwarded from './ic_phone_forwarded_48px.svg'
+import phone_in_talk from './ic_phone_in_talk_48px.svg'
+import phone_locked from './ic_phone_locked_48px.svg'
+import phone_missed from './ic_phone_missed_48px.svg'
+import phone_paused from './ic_phone_paused_48px.svg'
+import power from './ic_power_48px.svg'
+import priority_high from './ic_priority_high_48px.svg'
+import rv_hookup from './ic_rv_hookup_48px.svg'
+import sd_card from './ic_sd_card_48px.svg'
+import sim_card_alert from './ic_sim_card_alert_48px.svg'
+import sms from './ic_sms_48px.svg'
+import sms_failed from './ic_sms_failed_48px.svg'
+import sync from './ic_sync_48px.svg'
+import sync_disabled from './ic_sync_disabled_48px.svg'
+import sync_problem from './ic_sync_problem_48px.svg'
+import system_update from './ic_system_update_48px.svg'
+import tap_and_play from './ic_tap_and_play_48px.svg'
+import time_to_leave from './ic_time_to_leave_48px.svg'
+import vibration from './ic_vibration_48px.svg'
+import voice_chat from './ic_voice_chat_48px.svg'
+import vpn_lock from './ic_vpn_lock_48px.svg'
+import wc from './ic_wc_48px.svg'
+import wifi from './ic_wifi_48px.svg'
+
+const icons = {
+    folder_special,
+    airline_seat_flat_angled,
+    airline_seat_legroom_extra,
+    do_not_disturb_off,
+    airline_seat_recline_extra,
+    do_not_disturb_alt,
+    airline_seat_flat,
+    more,
+    enhanced_encryption,
+    airline_seat_legroom_reduced,
+    confirmation_number,
+    do_not_disturb,
+    drive_eta,
+    event_available,
+    bluetooth_audio,
+    disc_full,
+    airline_seat_recline_normal,
+    live_tv,
+    event_busy,
+    airline_seat_individual_suite,
+    adb,
+    do_not_disturb_on,
+    airline_seat_legroom_normal,
+    event_note,
+    mms,
+    network_check,
+    network_locked,
+    no_encryption,
+    ondemand_video,
+    personal_video,
+    phone_bluetooth_speaker,
+    phone_forwarded,
+    phone_in_talk,
+    phone_locked,
+    phone_missed,
+    phone_paused,
+    power,
+    priority_high,
+    rv_hookup,
+    sd_card,
+    sim_card_alert,
+    sms,
+    sms_failed,
+    sync,
+    sync_disabled,
+    sync_problem,
+    system_update,
+    tap_and_play,
+    time_to_leave,
+    vibration,
+    voice_chat,
+    vpn_lock,
+    wc,
+    wifi
+}
+
+export default {
+    icons,
+    names: Object.keys(icons)
+}
