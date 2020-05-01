5e13db62485ab87fbfa309945c2d0baae82b02eb
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-05-01 22:18:33 +0400

    add maps icons

---
+++ src/icons/maps/index.js
@@ -0,0 +1,144 @@
+import directions_railway from './ic_directions_railway_48px.svg'
+import directions from './ic_directions_48px.svg'
+import edit_location from './ic_edit_location_48px.svg'
+import restaurant_menu from './ic_restaurant_menu_48px.svg'
+import local_activity from './ic_local_activity_48px.svg'
+import local_car_wash from './ic_local_car_wash_48px.svg'
+import navigation from './ic_navigation_48px.svg'
+import local_parking from './ic_local_parking_48px.svg'
+import local_florist from './ic_local_florist_48px.svg'
+import local_hospital from './ic_local_hospital_48px.svg'
+import local_printshop from './ic_local_printshop_48px.svg'
+import local_pharmacy from './ic_local_pharmacy_48px.svg'
+import tram from './ic_tram_48px.svg'
+import directions_boat from './ic_directions_boat_48px.svg'
+import local_atm from './ic_local_atm_48px.svg'
+import terrain from './ic_terrain_48px.svg'
+import pin_drop from './ic_pin_drop_48px.svg'
+import place from './ic_place_48px.svg'
+import streetview from './ic_streetview_48px.svg'
+import _map from './ic_map_48px.svg'
+import restaurant from './ic_restaurant_48px.svg'
+import directions_transit from './ic_directions_transit_48px.svg'
+import near_me from './ic_near_me_48px.svg'
+import local_taxi from './ic_local_taxi_48px.svg'
+import local_laundry_service from './ic_local_laundry_service_48px.svg'
+import local_library from './ic_local_library_48px.svg'
+import local_drink from './ic_local_drink_48px.svg'
+import local_grocery_store from './ic_local_grocery_store_48px.svg'
+import directions_car from './ic_directions_car_48px.svg'
+import local_shipping from './ic_local_shipping_48px.svg'
+import directions_bike from './ic_directions_bike_48px.svg'
+import local_offer from './ic_local_offer_48px.svg'
+import local_hotel from './ic_local_hotel_48px.svg'
+import hotel from './ic_hotel_48px.svg'
+import flight from './ic_flight_48px.svg'
+import layers_clear from './ic_layers_clear_48px.svg'
+import local_phone from './ic_local_phone_48px.svg'
+import directions_walk from './ic_directions_walk_48px.svg'
+import directions_bus from './ic_directions_bus_48px.svg'
+import local_convenience_store from './ic_local_convenience_store_48px.svg'
+import directions_subway from './ic_directions_subway_48px.svg'
+import directions_run from './ic_directions_run_48px.svg'
+import local_movies from './ic_local_movies_48px.svg'
+import my_location from './ic_my_location_48px.svg'
+import person_pin_circle from './ic_person_pin_circle_48px.svg'
+import store_mall_directory from './ic_store_mall_directory_48px.svg'
+import local_gas_station from './ic_local_gas_station_48px.svg'
+import satellite from './ic_satellite_48px.svg'
+import add_location from './ic_add_location_48px.svg'
+import local_bar from './ic_local_bar_48px.svg'
+import train from './ic_train_48px.svg'
+import rate_review from './ic_rate_review_48px.svg'
+import local_pizza from './ic_local_pizza_48px.svg'
+import local_mall from './ic_local_mall_48px.svg'
+import person_pin from './ic_person_pin_48px.svg'
+import local_cafe from './ic_local_cafe_48px.svg'
+import transfer_within_a_station from './ic_transfer_within_a_station_48px.svg'
+import local_dining from './ic_local_dining_48px.svg'
+import local_airport from './ic_local_airport_48px.svg'
+import local_see from './ic_local_see_48px.svg'
+import ev_station from './ic_ev_station_48px.svg'
+import subway from './ic_subway_48px.svg'
+import layers from './ic_layers_48px.svg'
+import traffic from './ic_traffic_48px.svg'
+import local_play from './ic_local_play_48px.svg'
+import local_post_office from './ic_local_post_office_48px.svg'
+import beenhere from './ic_beenhere_48px.svg'
+import zoom_out_map from './ic_zoom_out_map_48px.svg'
+
+const icons = {
+    directions_railway,
+    directions,
+    edit_location,
+    restaurant_menu,
+    local_activity,
+    local_car_wash,
+    navigation,
+    local_parking,
+    local_florist,
+    local_hospital,
+    local_printshop,
+    local_pharmacy,
+    tram,
+    directions_boat,
+    local_atm,
+    terrain,
+    pin_drop,
+    place,
+    streetview,
+    _map,
+    restaurant,
+    directions_transit,
+    near_me,
+    local_taxi,
+    local_laundry_service,
+    local_library,
+    local_drink,
+    local_grocery_store,
+    directions_car,
+    local_shipping,
+    directions_bike,
+    local_offer,
+    local_hotel,
+    hotel,
+    flight,
+    layers_clear,
+    local_phone,
+    directions_walk,
+    directions_bus,
+    local_convenience_store,
+    directions_subway,
+    directions_run,
+    local_movies,
+    my_location,
+    person_pin_circle,
+    store_mall_directory,
+    local_gas_station,
+    satellite,
+    add_location,
+    local_bar,
+    train,
+    rate_review,
+    local_pizza,
+    local_mall,
+    person_pin,
+    local_cafe,
+    transfer_within_a_station,
+    local_dining,
+    local_airport,
+    local_see,
+    ev_station,
+    subway,
+    layers,
+    traffic,
+    local_play,
+    local_post_office,
+    beenhere,
+    zoom_out_map
+}
+
+export default {
+    icons,
+    names: Object.keys(icons)
+}
