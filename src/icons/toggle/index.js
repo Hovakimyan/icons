7dc393c8d365fe030be1fa09c25f4c4a9fcafb65
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-05-01 22:19:35 +0400

    add toggle icons

---
+++ src/icons/toggle/index.js
@@ -0,0 +1,24 @@
+import check_box from './ic_check_box_48px.svg'
+import check_box_outline_blank from './ic_check_box_outline_blank_48px.svg'
+import indeterminate_check_box from './ic_indeterminate_check_box_24px.svg'
+import radio_button_checked from './ic_radio_button_checked_48px.svg'
+import radio_button_unchecked from './ic_radio_button_unchecked_48px.svg'
+import star from './ic_star_24px.svg'
+import star_border from './ic_star_border_24px.svg'
+import star_half from './ic_star_half_48px.svg'
+
+const icons = {
+    check_box,
+    check_box_outline_blank,
+    indeterminate_check_box,
+    radio_button_checked,
+    radio_button_unchecked,
+    star,
+    star_border,
+    star_half
+}
+
+export default {
+    icons,
+    names: Object.keys(icons)
+}
