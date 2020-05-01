e521f9cb892243f4d1bf8f5b0e40caf0092e34b2
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-05-01 22:17:16 +0400

    add communications icosn

---
+++ src/icons/communication/index.js
@@ -0,0 +1,108 @@
+import business from './ic_business_48px.svg'
+import call from './ic_call_48px.svg'
+import call_end from './ic_call_end_48px.svg'
+import call_made from './ic_call_made_48px.svg'
+import call_merge from './ic_call_merge_48px.svg'
+import call_missed from './ic_call_missed_48px.svg'
+import call_missed_outgoing from './ic_call_missed_outgoing_48px.svg'
+import call_received from './ic_call_received_48px.svg'
+import call_split from './ic_call_split_48px.svg'
+import chat from './ic_chat_48px.svg'
+import chat_bubble from './ic_chat_bubble_48px.svg'
+import chat_bubble_outline from './ic_chat_bubble_outline_48px.svg'
+import clear_all from './ic_clear_all_48px.svg'
+import comment from './ic_comment_48px.svg'
+import contact_mail from './ic_contact_mail_48px.svg'
+import contact_phone from './ic_contact_phone_48px.svg'
+import contacts from './ic_contacts_48px.svg'
+import dialer_sip from './ic_dialer_sip_48px.svg'
+import dialpad from './ic_dialpad_48px.svg'
+import email from './ic_email_48px.svg'
+import forum from './ic_forum_48px.svg'
+import import_contacts from './ic_import_contacts_48px.svg'
+import import_export from './ic_import_export_48px.svg'
+import invert_colors_off from './ic_invert_colors_off_48px.svg'
+import live_help from './ic_live_help_48px.svg'
+import location_off from './ic_location_off_48px.svg'
+import location_on from './ic_location_on_48px.svg'
+import mail_outline from './ic_mail_outline_48px.svg'
+import message from './ic_message_48px.svg'
+import no_sim from './ic_no_sim_48px.svg'
+import phone from './ic_phone_48px.svg'
+import phonelink_erase from './ic_phonelink_erase_24px.svg'
+import phonelink_lock from './ic_phonelink_lock_24px.svg'
+import phonelink_ring from './ic_phonelink_ring_24px.svg'
+import phonelink_setup from './ic_phonelink_setup_24px.svg'
+import portable_wifi_off from './ic_portable_wifi_off_48px.svg'
+import present_to_all from './ic_present_to_all_48px.svg'
+import ring_volume from './ic_ring_volume_48px.svg'
+import rss_feed from './ic_rss_feed_48px.svg'
+import screen_share from './ic_screen_share_48px.svg'
+import speaker_phone from './ic_speaker_phone_48px.svg'
+import stay_current_landscape from './ic_stay_current_landscape_48px.svg'
+import stay_current_portrait from './ic_stay_current_portrait_48px.svg'
+import stay_primary_landscape from './ic_stay_primary_landscape_48px.svg'
+import stay_primary_portrait from './ic_stay_primary_portrait_48px.svg'
+import stop_screen_share from './ic_stop_screen_share_48px.svg'
+import swap_calls from './ic_swap_calls_48px.svg'
+import textsms from './ic_textsms_48px.svg'
+import voicemail from './ic_voicemail_48px.svg'
+import vpn_key from './ic_vpn_key_48px.svg'
+
+const icons = {
+    business,
+    call,
+    call_end,
+    call_made,
+    call_merge,
+    call_missed,
+    call_missed_outgoing,
+    call_received,
+    call_split,
+    chat,
+    chat_bubble,
+    chat_bubble_outline,
+    clear_all,
+    comment,
+    contact_mail,
+    contact_phone,
+    contacts,
+    dialer_sip,
+    dialpad,
+    email,
+    forum,
+    import_contacts,
+    import_export,
+    invert_colors_off,
+    live_help,
+    location_off,
+    location_on,
+    mail_outline,
+    message,
+    no_sim,
+    phone,
+    phonelink_erase,
+    phonelink_lock,
+    phonelink_ring,
+    phonelink_setup,
+    portable_wifi_off,
+    present_to_all,
+    ring_volume,
+    rss_feed,
+    screen_share,
+    speaker_phone,
+    stay_current_landscape,
+    stay_current_portrait,
+    stay_primary_landscape,
+    stay_primary_portrait,
+    stop_screen_share,
+    swap_calls,
+    textsms,
+    voicemail,
+    vpn_key,
+}
+
+export default {
+    icons,
+    names: Object.keys(icons)
+}
