24c4723864b57d33710b1a54e4a0e502ccf01aa9
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-05-01 22:16:32 +0400

    add actions icons

---
+++ src/icons/action/index.js
@@ -0,0 +1,426 @@
+import rotation_3d from './ic_3d_rotation_48px.svg'
+import accessibility from './ic_accessibility_48px.svg'
+import accessible from './ic_accessible_48px.svg'
+import account_balance from './ic_account_balance_48px.svg'
+import account_balance_wallet from './ic_account_balance_wallet_48px.svg'
+import account_box from './ic_account_box_48px.svg'
+import account_circle from './ic_account_circle_48px.svg'
+import add_shopping_cart from './ic_add_shopping_cart_48px.svg'
+import alarm from './ic_alarm_48px.svg'
+import alarm_add from './ic_alarm_add_48px.svg'
+import alarm_off from './ic_alarm_off_48px.svg'
+import alarm_on from './ic_alarm_on_48px.svg'
+import all_out from './ic_all_out_48px.svg'
+import android from './ic_android_48px.svg'
+import announcement from './ic_announcement_48px.svg'
+import aspect_ratio from './ic_aspect_ratio_48px.svg'
+import assessment from './ic_assessment_48px.svg'
+import assignment from './ic_assignment_48px.svg'
+import assignment_ind from './ic_assignment_ind_48px.svg'
+import assignment_late from './ic_assignment_late_48px.svg'
+import assignment_return from './ic_assignment_return_48px.svg'
+import assignment_returned from './ic_assignment_returned_48px.svg'
+import assignment_turned_in from './ic_assignment_turned_in_48px.svg'
+import autorenew from './ic_autorenew_48px.svg'
+import backup from './ic_backup_48px.svg'
+import book from './ic_book_48px.svg'
+import bookmark from './ic_bookmark_48px.svg'
+import bookmark_border from './ic_bookmark_border_48px.svg'
+import bug_report from './ic_bug_report_48px.svg'
+import build from './ic_build_48px.svg'
+import cached from './ic_cached_48px.svg'
+import card_giftcard from './ic_card_giftcard_48px.svg'
+import card_membership from './ic_card_membership_48px.svg'
+import card_travel from './ic_card_travel_48px.svg'
+import change_history from './ic_change_history_48px.svg'
+import check_circle from './ic_check_circle_48px.svg'
+import chrome_reader_mode from './ic_chrome_reader_mode_48px.svg'
+import ic_class from './ic_class_48px.svg'
+import code from './ic_code_48px.svg'
+import compare_arrows from './ic_compare_arrows_48px.svg'
+import copyright from './ic_copyright_48px.svg'
+import credit_card from './ic_credit_card_48px.svg'
+import dashboard from './ic_dashboard_48px.svg'
+import date_range from './ic_date_range_48px.svg'
+import ic_delete from './ic_delete_48px.svg'
+import delete_forever from './ic_delete_forever_48px.svg'
+import description from './ic_description_48px.svg'
+import dns from './ic_dns_48px.svg'
+import done from './ic_done_48px.svg'
+import done_all from './ic_done_all_48px.svg'
+import donut_large from './ic_donut_large_48px.svg'
+import donut_small from './ic_donut_small_48px.svg'
+import euro_symbol from './ic_euro_symbol_48px.svg'
+import event from './ic_event_48px.svg'
+import event_seat from './ic_event_seat_48px.svg'
+import exit_to_app from './ic_exit_to_app_48px.svg'
+import explore from './ic_explore_48px.svg'
+import extension from './ic_extension_48px.svg'
+import face from './ic_face_48px.svg'
+import favorite from './ic_favorite_48px.svg'
+import favorite_border from './ic_favorite_border_48px.svg'
+import feedback from './ic_feedback_48px.svg'
+import find_in_page from './ic_find_in_page_48px.svg'
+import find_replace from './ic_find_replace_48px.svg'
+import fingerprint from './ic_fingerprint_48px.svg'
+import flight_land from './ic_flight_land_48px.svg'
+import flight_takeoff from './ic_flight_takeoff_48px.svg'
+import flip_to_back from './ic_flip_to_back_48px.svg'
+import flip_to_front from './ic_flip_to_front_48px.svg'
+import g_translate from './ic_g_translate_48px.svg'
+import gavel from './ic_gavel_48px.svg'
+import get_app from './ic_get_app_48px.svg'
+import grade from './ic_grade_48px.svg'
+import group_work from './ic_group_work_48px.svg'
+import help from './ic_help_48px.svg'
+import highlight_off from './ic_highlight_off_48px.svg'
+import history from './ic_history_48px.svg'
+import home from './ic_home_48px.svg'
+import hourglass_empty from './ic_hourglass_empty_48px.svg'
+import hourglass_full from './ic_hourglass_full_48px.svg'
+import http from './ic_http_48px.svg'
+import https from './ic_https_48px.svg'
+import important_devices from './ic_important_devices_48px.svg'
+import info from './ic_info_48px.svg'
+import info_outline from './ic_info_outline_48px.svg'
+import input from './ic_input_48px.svg'
+import invert_colors from './ic_invert_colors_48px.svg'
+import label from './ic_label_48px.svg'
+import label_outline from './ic_label_outline_48px.svg'
+import language from './ic_language_48px.svg'
+import launch from './ic_launch_48px.svg'
+import lightbulb_outline from './ic_lightbulb_outline_48px.svg'
+import line_style from './ic_line_style_48px.svg'
+import line_weight from './ic_line_weight_48px.svg'
+import list from './ic_list_48px.svg'
+import lock from './ic_lock_48px.svg'
+import lock_open from './ic_lock_open_48px.svg'
+import lock_outline from './ic_lock_outline_48px.svg'
+import loyalty from './ic_loyalty_48px.svg'
+import markunread_mailbox from './ic_markunread_mailbox_48px.svg'
+import motorcycle from './ic_motorcycle_48px.svg'
+import note_add from './ic_note_add_48px.svg'
+import opacity from './ic_opacity_48px.svg'
+import open_in_browser from './ic_open_in_browser_48px.svg'
+import open_in_new from './ic_open_in_new_48px.svg'
+import open_with from './ic_open_with_48px.svg'
+import pageview from './ic_pageview_48px.svg'
+import pan_tool from './ic_pan_tool_48px.svg'
+import payment from './ic_payment_48px.svg'
+import perm_camera_mic from './ic_perm_camera_mic_48px.svg'
+import perm_contact_calendar from './ic_perm_contact_calendar_48px.svg'
+import perm_data_setting from './ic_perm_data_setting_48px.svg'
+import perm_device_information from './ic_perm_device_information_48px.svg'
+import perm_identity from './ic_perm_identity_48px.svg'
+import perm_media from './ic_perm_media_48px.svg'
+import perm_phone_msg from './ic_perm_phone_msg_48px.svg'
+import perm_scan_wifi from './ic_perm_scan_wifi_48px.svg'
+import pets from './ic_pets_48px.svg'
+import picture_in_picture from './ic_picture_in_picture_48px.svg'
+import picture_in_picture_alt from './ic_picture_in_picture_alt_48px.svg'
+import play_for_work from './ic_play_for_work_48px.svg'
+import polymer from './ic_polymer_48px.svg'
+import power_settings_new from './ic_power_settings_new_48px.svg'
+import pregnant_woman from './ic_pregnant_woman_48px.svg'
+import print from './ic_print_48px.svg'
+import query_builder from './ic_query_builder_48px.svg'
+import question_answer from './ic_question_answer_48px.svg'
+import receipt from './ic_receipt_48px.svg'
+import record_voice_over from './ic_record_voice_over_48px.svg'
+import redeem from './ic_redeem_48px.svg'
+import remove_shopping_cart from './ic_remove_shopping_cart_48px.svg'
+import report_problem from './ic_report_problem_48px.svg'
+import restore from './ic_restore_48px.svg'
+import restore_page from './ic_restore_page_48px.svg'
+import room from './ic_room_48px.svg'
+import rounded_corner from './ic_rounded_corner_48px.svg'
+import rowing from './ic_rowing_48px.svg'
+import schedule from './ic_schedule_48px.svg'
+import search from './ic_search_48px.svg'
+import settings from './ic_settings_48px.svg'
+import settings_applications from './ic_settings_applications_48px.svg'
+import settings_backup_restore from './ic_settings_backup_restore_48px.svg'
+import settings_bluetooth from './ic_settings_bluetooth_48px.svg'
+import settings_brightness from './ic_settings_brightness_48px.svg'
+import settings_cell from './ic_settings_cell_48px.svg'
+import settings_ethernet from './ic_settings_ethernet_48px.svg'
+import settings_input_antenna from './ic_settings_input_antenna_48px.svg'
+import settings_input_component from './ic_settings_input_component_48px.svg'
+import settings_input_composite from './ic_settings_input_composite_48px.svg'
+import settings_input_hdmi from './ic_settings_input_hdmi_48px.svg'
+import settings_input_svideo from './ic_settings_input_svideo_48px.svg'
+import settings_overscan from './ic_settings_overscan_48px.svg'
+import settings_phone from './ic_settings_phone_48px.svg'
+import settings_power from './ic_settings_power_48px.svg'
+import settings_remote from './ic_settings_remote_48px.svg'
+import settings_voice from './ic_settings_voice_48px.svg'
+import shop from './ic_shop_48px.svg'
+import shop_two from './ic_shop_two_48px.svg'
+import shopping_basket from './ic_shopping_basket_48px.svg'
+import shopping_cart from './ic_shopping_cart_48px.svg'
+import speaker_notes from './ic_speaker_notes_48px.svg'
+import speaker_notes_off from './ic_speaker_notes_off_48px.svg'
+import spellcheck from './ic_spellcheck_48px.svg'
+import stars from './ic_stars_48px.svg'
+import store from './ic_store_48px.svg'
+import subject from './ic_subject_48px.svg'
+import supervisor_account from './ic_supervisor_account_48px.svg'
+import swap_horiz from './ic_swap_horiz_48px.svg'
+import swap_vert from './ic_swap_vert_48px.svg'
+import swap_vertical_circle from './ic_swap_vertical_circle_48px.svg'
+import system_update_alt from './ic_system_update_alt_48px.svg'
+import tab from './ic_tab_48px.svg'
+import tab_unselected from './ic_tab_unselected_48px.svg'
+import theaters from './ic_theaters_48px.svg'
+import thumb_down from './ic_thumb_down_48px.svg'
+import thumb_up from './ic_thumb_up_48px.svg'
+import thumbs_up_down from './ic_thumbs_up_down_48px.svg'
+import timeline from './ic_timeline_48px.svg'
+import toc from './ic_toc_48px.svg'
+import today from './ic_today_48px.svg'
+import toll from './ic_toll_48px.svg'
+import touch_app from './ic_touch_app_48px.svg'
+import track_changes from './ic_track_changes_48px.svg'
+import translate from './ic_translate_48px.svg'
+import trending_down from './ic_trending_down_48px.svg'
+import trending_flat from './ic_trending_flat_48px.svg'
+import trending_up from './ic_trending_up_48px.svg'
+import turned_in from './ic_turned_in_48px.svg'
+import turned_in_not from './ic_turned_in_not_48px.svg'
+import update from './ic_update_48px.svg'
+import verified_user from './ic_verified_user_48px.svg'
+import view_agenda from './ic_view_agenda_48px.svg'
+import view_array from './ic_view_array_48px.svg'
+import view_carousel from './ic_view_carousel_48px.svg'
+import view_column from './ic_view_column_48px.svg'
+import view_day from './ic_view_day_48px.svg'
+import view_headline from './ic_view_headline_48px.svg'
+import view_list from './ic_view_list_48px.svg'
+import view_module from './ic_view_module_48px.svg'
+import view_quilt from './ic_view_quilt_48px.svg'
+import view_stream from './ic_view_stream_48px.svg'
+import view_week from './ic_view_week_48px.svg'
+import visibility from './ic_visibility_48px.svg'
+import visibility_off from './ic_visibility_off_48px.svg'
+import watch_later from './ic_watch_later_48px.svg'
+import work from './ic_work_48px.svg'
+import youtube_searched_for from './ic_youtube_searched_for_48px.svg'
+import zoom_in from './ic_zoom_in_24px.svg'
+import zoom_out from './ic_zoom_out_24px.svg'
+
+const icons = {
+    rotation_3d,
+    accessibility,
+    accessible,
+    account_balance,
+    account_balance_wallet,
+    account_box,
+    account_circle,
+    add_shopping_cart,
+    alarm,
+    alarm_add,
+    alarm_off,
+    alarm_on,
+    all_out,
+    android,
+    announcement,
+    aspect_ratio,
+    assessment,
+    assignment,
+    assignment_ind,
+    assignment_late,
+    assignment_return,
+    assignment_returned,
+    assignment_turned_in,
+    autorenew,
+    backup,
+    book,
+    bookmark,
+    bookmark_border,
+    bug_report,
+    build,
+    cached,
+    card_giftcard,
+    card_membership,
+    card_travel,
+    change_history,
+    check_circle,
+    chrome_reader_mode,
+    ic_class,
+    code,
+    compare_arrows,
+    copyright,
+    credit_card,
+    dashboard,
+    date_range,
+    ic_delete,
+    delete_forever,
+    description,
+    dns,
+    done,
+    done_all,
+    donut_large,
+    donut_small,
+    euro_symbol,
+    event,
+    event_seat,
+    exit_to_app,
+    explore,
+    extension,
+    face,
+    favorite,
+    favorite_border,
+    feedback,
+    find_in_page,
+    find_replace,
+    fingerprint,
+    flight_land,
+    flight_takeoff,
+    flip_to_back,
+    flip_to_front,
+    g_translate,
+    gavel,
+    get_app,
+    grade,
+    group_work,
+    help,
+    highlight_off,
+    history,
+    home,
+    hourglass_empty,
+    hourglass_full,
+    http,
+    https,
+    important_devices,
+    info,
+    info_outline,
+    input,
+    invert_colors,
+    label,
+    label_outline,
+    language,
+    launch,
+    lightbulb_outline,
+    line_style,
+    line_weight,
+    list,
+    lock,
+    lock_open,
+    lock_outline,
+    loyalty,
+    markunread_mailbox,
+    motorcycle,
+    note_add,
+    opacity,
+    open_in_browser,
+    open_in_new,
+    open_with,
+    pageview,
+    pan_tool,
+    payment,
+    perm_camera_mic,
+    perm_contact_calendar,
+    perm_data_setting,
+    perm_device_information,
+    perm_identity,
+    perm_media,
+    perm_phone_msg,
+    perm_scan_wifi,
+    pets,
+    picture_in_picture,
+    picture_in_picture_alt,
+    play_for_work,
+    polymer,
+    power_settings_new,
+    pregnant_woman,
+    print,
+    query_builder,
+    question_answer,
+    receipt,
+    record_voice_over,
+    redeem,
+    remove_shopping_cart,
+    report_problem,
+    restore,
+    restore_page,
+    room,
+    rounded_corner,
+    rowing,
+    schedule,
+    search,
+    settings,
+    settings_applications,
+    settings_backup_restore,
+    settings_bluetooth,
+    settings_brightness,
+    settings_cell,
+    settings_ethernet,
+    settings_input_antenna,
+    settings_input_component,
+    settings_input_composite,
+    settings_input_hdmi,
+    settings_input_svideo,
+    settings_overscan,
+    settings_phone,
+    settings_power,
+    settings_remote,
+    settings_voice,
+    shop,
+    shop_two,
+    shopping_basket,
+    shopping_cart,
+    speaker_notes,
+    speaker_notes_off,
+    spellcheck,
+    stars,
+    store,
+    subject,
+    supervisor_account,
+    swap_horiz,
+    swap_vert,
+    swap_vertical_circle,
+    system_update_alt,
+    tab,
+    tab_unselected,
+    theaters,
+    thumb_down,
+    thumb_up,
+    thumbs_up_down,
+    timeline,
+    toc,
+    today,
+    toll,
+    touch_app,
+    track_changes,
+    translate,
+    trending_down,
+    trending_flat,
+    trending_up,
+    turned_in,
+    turned_in_not,
+    update,
+    verified_user,
+    view_agenda,
+    view_array,
+    view_carousel,
+    view_column,
+    view_day,
+    view_headline,
+    view_list,
+    view_module,
+    view_quilt,
+    view_stream,
+    view_week,
+    visibility,
+    visibility_off,
+    watch_later,
+    work,
+    youtube_searched_for,
+    zoom_in,
+    zoom_out,
+}
+
+export default {
+    icons,
+    names: Object.keys(icons)
+}
