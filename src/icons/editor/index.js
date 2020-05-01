3d8139125f7268574ed52b73d860dfc7f3bcbfff
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-05-01 22:17:51 +0400

    add editor icons

---
+++ src/icons/editor/index.js
@@ -0,0 +1,142 @@
+import format_textdirection_l_to_r from './ic_format_textdirection_l_to_r_48px.svg'
+import format_align_left from './ic_format_align_left_48px.svg'
+import border_all from './ic_border_all_48px.svg'
+import format_color_text from './ic_format_color_text_48px.svg'
+import format_textdirection_r_to_l from './ic_format_textdirection_r_to_l_48px.svg'
+import functions from './ic_functions_48px.svg'
+import format_color_reset from './ic_format_color_reset_48px.svg'
+import format_align_right from './ic_format_align_right_48px.svg'
+import insert_emoticon from './ic_insert_emoticon_48px.svg'
+import format_quote from './ic_format_quote_48px.svg'
+import format_align_justify from './ic_format_align_justify_48px.svg'
+import attach_file from './ic_attach_file_48px.svg'
+import format_indent_decrease from './ic_format_indent_decrease_48px.svg'
+import format_italic from './ic_format_italic_48px.svg'
+import insert_drive_file from './ic_insert_drive_file_48px.svg'
+import insert_link from './ic_insert_link_48px.svg'
+import border_bottom from './ic_border_bottom_48px.svg'
+import format_size from './ic_format_size_48px.svg'
+import format_indent_increase from './ic_format_indent_increase_48px.svg'
+import border_color from './ic_border_color_48px.svg'
+import format_shapes from './ic_format_shapes_48px.svg'
+import format_bold from './ic_format_bold_48px.svg'
+import border_left from './ic_border_left_48px.svg'
+import border_clear from './ic_border_clear_48px.svg'
+import format_underlined from './ic_format_underlined_48px.svg'
+import border_horizontal from './ic_border_horizontal_48px.svg'
+import format_color_fill from './ic_format_color_fill_48px.svg'
+import format_strikethrough from './ic_format_strikethrough_48px.svg'
+import format_paint from './ic_format_paint_48px.svg'
+import bubble_chart from './ic_bubble_chart_48px.svg'
+import border_top from './ic_border_top_48px.svg'
+import insert_invitation from './ic_insert_invitation_48px.svg'
+import format_list_numbered from './ic_format_list_numbered_48px.svg'
+import border_outer from './ic_border_outer_48px.svg'
+import border_vertical from './ic_border_vertical_48px.svg'
+import linear_scale from './ic_linear_scale_48px.svg'
+import border_inner from './ic_border_inner_48px.svg'
+import format_align_center from './ic_format_align_center_48px.svg'
+import drag_handle from './ic_drag_handle_48px.svg'
+import format_clear from './ic_format_clear_48px.svg'
+import border_style from './ic_border_style_48px.svg'
+import attach_money from './ic_attach_money_48px.svg'
+import highlight from './ic_highlight_48px.svg'
+import border_right from './ic_border_right_48px.svg'
+import format_line_spacing from './ic_format_line_spacing_48px.svg'
+import insert_comment from './ic_insert_comment_48px.svg'
+import format_list_bulleted from './ic_format_list_bulleted_48px.svg'
+import insert_chart from './ic_insert_chart_48px.svg'
+import merge_type from './ic_merge_type_48px.svg'
+import insert_photo from './ic_insert_photo_48px.svg'
+import mode_comment from './ic_mode_comment_48px.svg'
+import mode_edit from './ic_mode_edit_48px.svg'
+import monetization_on from './ic_monetization_on_48px.svg'
+import multiline_chart from './ic_multiline_chart_48px.svg'
+import pie_chart from './ic_pie_chart_48px.svg'
+import pie_chart_outlined from './ic_pie_chart_outlined_48px.svg'
+import publish from './ic_publish_48px.svg'
+import short_text from './ic_short_text_48px.svg'
+import show_chart from './ic_show_chart_48px.svg'
+import strikethrough_s from './ic_strikethrough_s_48px.svg'
+import space_bar from './ic_space_bar_48px.svg'
+import text_fields from './ic_text_fields_48px.svg'
+import vertical_align_bottom from './ic_vertical_align_bottom_48px.svg'
+import vertical_align_center from './ic_vertical_align_center_48px.svg'
+import title from './ic_title_48px.svg'
+import vertical_align_top from './ic_vertical_align_top_48px.svg'
+import wrap_text from './ic_wrap_text_48px.svg'
+
+const icons = {
+    format_textdirection_l_to_r,
+    format_align_left,
+    border_all,
+    format_color_text,
+    format_textdirection_r_to_l,
+    functions,
+    format_color_reset,
+    format_align_right,
+    insert_emoticon,
+    format_quote,
+    format_align_justify,
+    attach_file,
+    format_indent_decrease,
+    format_italic,
+    insert_drive_file,
+    insert_link,
+    border_bottom,
+    format_size,
+    format_indent_increase,
+    border_color,
+    format_shapes,
+    format_bold,
+    border_left,
+    border_clear,
+    format_underlined,
+    border_horizontal,
+    format_color_fill,
+    format_strikethrough,
+    format_paint,
+    bubble_chart,
+    border_top,
+    insert_invitation,
+    format_list_numbered,
+    border_outer,
+    border_vertical,
+    linear_scale,
+    border_inner,
+    format_align_center,
+    drag_handle,
+    format_clear,
+    border_style,
+    attach_money,
+    highlight,
+    border_right,
+    format_line_spacing,
+    insert_comment,
+    format_list_bulleted,
+    insert_chart,
+    merge_type,
+    insert_photo,
+    mode_comment,
+    mode_edit,
+    monetization_on,
+    multiline_chart,
+    pie_chart,
+    pie_chart_outlined,
+    publish,
+    short_text,
+    show_chart,
+    strikethrough_s,
+    space_bar,
+    text_fields,
+    vertical_align_bottom,
+    vertical_align_center,
+    title,
+    vertical_align_top,
+    wrap_text
+}
+
+export default {
+    icons,
+    names: Object.keys(icons)
+}
