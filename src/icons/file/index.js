d1befd1570294246ee704b78c6fa5a1523bb7896
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-05-01 22:18:02 +0400

    add file icons

---
+++ src/icons/file/index.js
@@ -0,0 +1,36 @@
+import folder_shared from './ic_folder_shared_48px.svg'
+import folder_open from './ic_folder_open_48px.svg'
+import folder from './ic_folder_48px.svg'
+import file_upload from './ic_file_upload_48px.svg'
+import file_download from './ic_file_download_48px.svg'
+import create_new_folder from './ic_create_new_folder_48px.svg'
+import cloud_upload from './ic_cloud_upload_48px.svg'
+import cloud_queue from './ic_cloud_queue_48px.svg'
+import cloud_off from './ic_cloud_off_48px.svg'
+import cloud_download from './ic_cloud_download_48px.svg'
+import cloud_done from './ic_cloud_done_48px.svg'
+import cloud_circle from './ic_cloud_circle_48px.svg'
+import cloud from './ic_cloud_48px.svg'
+import attachment from './ic_attachment_48px.svg'
+
+const icons = {
+    folder_shared,
+    folder_open,
+    folder,
+    file_upload,
+    file_download,
+    create_new_folder,
+    cloud_upload,
+    cloud_queue,
+    cloud_off,
+    cloud_download,
+    cloud_done,
+    cloud_circle,
+    cloud,
+    attachment
+}
+
+export default {
+    icons,
+    names: Object.keys(icons)
+}
