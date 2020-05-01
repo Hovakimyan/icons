0ab2a6089b3421ecb659735e3205ce50cc4c14ca
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-05-01 22:19:22 +0400

    add places icons

---
+++ src/icons/places/index.js
@@ -0,0 +1,46 @@
+import fitness_center from './ic_fitness_center_48px.svg'
+import kitchen from './ic_kitchen_48px.svg'
+import child_care from './ic_child_care_48px.svg'
+import ac_unit from './ic_ac_unit_48px.svg'
+import child_friendly from './ic_child_friendly_48px.svg'
+import all_inclusive from './ic_all_inclusive_48px.svg'
+import hot_tub from './ic_hot_tub_48px.svg'
+import room_service from './ic_room_service_48px.svg'
+import casino from './ic_casino_48px.svg'
+import airport_shuttle from './ic_airport_shuttle_48px.svg'
+import golf_course from './ic_golf_course_48px.svg'
+import business_center from './ic_business_center_48px.svg'
+import beach_access from './ic_beach_access_48px.svg'
+import free_breakfast from './ic_free_breakfast_48px.svg'
+import pool from './ic_pool_48px.svg'
+import rv_hookup from './ic_rv_hookup_48px.svg'
+import smoke_free from './ic_smoke_free_48px.svg'
+import smoking_rooms from './ic_smoking_rooms_48px.svg'
+import spa from './ic_spa_48px.svg'
+
+const icons = {
+    fitness_center,
+    kitchen,
+    child_care,
+    ac_unit,
+    child_friendly,
+    all_inclusive,
+    hot_tub,
+    room_service,
+    casino,
+    airport_shuttle,
+    golf_course,
+    business_center,
+    beach_access,
+    free_breakfast,
+    pool,
+    rv_hookup,
+    smoke_free,
+    smoking_rooms,
+    spa
+}
+
+export default {
+    icons,
+    names: Object.keys(icons)
+}
