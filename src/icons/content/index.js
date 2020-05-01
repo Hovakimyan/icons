a312d0b885c7ecfc544dd2d1d5e57c7257edf79c
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-05-01 22:17:30 +0400

    add content icons

---
+++ src/icons/content/index.js
@@ -0,0 +1,90 @@
+import archive from './ic_archive_48px.svg'
+import clear from './ic_clear_48px.svg'
+import select_all from './ic_select_all_48px.svg'
+import forward from './ic_forward_48px.svg'
+import add from './ic_add_48px.svg'
+import content_cut from './ic_content_cut_48px.svg'
+import delete_sweep from './ic_delete_sweep_48px.svg'
+import unarchive from './ic_unarchive_48px.svg'
+import font_download from './ic_font_download_24px.svg'
+import sort from './ic_sort_48px.svg'
+import low_priority from './ic_low_priority_48px.svg'
+import content_copy from './ic_content_copy_48px.svg'
+import undo from './ic_undo_48px.svg'
+import link from './ic_link_48px.svg'
+import save from './ic_save_48px.svg'
+import create from './ic_create_48px.svg'
+import add_circle from './ic_add_circle_48px.svg'
+import backspace from './ic_backspace_48px.svg'
+import text_format from './ic_text_format_48px.svg'
+import add_circle_outline from './ic_add_circle_outline_48px.svg'
+import content_paste from './ic_content_paste_48px.svg'
+import gesture from './ic_gesture_48px.svg'
+import drafts from './ic_drafts_48px.svg'
+import filter_list from './ic_filter_list_48px.svg'
+import weekend from './ic_weekend_48px.svg'
+import inbox from './ic_inbox_48px.svg'
+import flag from './ic_flag_48px.svg'
+import send from './ic_send_48px.svg'
+import add_box from './ic_add_box_48px.svg'
+import block from './ic_block_48px.svg'
+import mail from './ic_mail_48px.svg'
+import markunread from './ic_markunread_48px.svg'
+import move_to_inbox from './ic_move_to_inbox_48px.svg'
+import next_week from './ic_next_week_48px.svg'
+import redo from './ic_redo_48px.svg'
+import remove from './ic_remove_48px.svg'
+import remove_circle from './ic_remove_circle_48px.svg'
+import remove_circle_outline from './ic_remove_circle_outline_48px.svg'
+import reply_all from './ic_reply_all_48px.svg'
+import reply from './ic_reply_48px.svg'
+import report from './ic_report_48px.svg'
+
+const icons = {
+    archive,
+    clear,
+    select_all,
+    forward,
+    add,
+    content_cut,
+    delete_sweep,
+    unarchive,
+    font_download,
+    sort,
+    low_priority,
+    content_copy,
+    undo,
+    link,
+    save,
+    create,
+    add_circle,
+    backspace,
+    text_format,
+    add_circle_outline,
+    content_paste,
+    gesture,
+    drafts,
+    filter_list,
+    weekend,
+    inbox,
+    flag,
+    send,
+    add_box,
+    block,
+    mail,
+    markunread,
+    move_to_inbox,
+    next_week,
+    redo,
+    remove,
+    remove_circle,
+    remove_circle_outline,
+    reply_all,
+    reply,
+    report
+}
+
+export default {
+    icons,
+    names: Object.keys(icons)
+}
