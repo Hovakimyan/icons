bfca9918656d31f04596806cb4003b120e83ef21
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-05-01 22:16:48 +0400

    add alert actions

---
+++ src/icons/alert/index.js
@@ -0,0 +1,16 @@
+import add_alert from './ic_add_alert_48px.svg'
+import error from './ic_error_48px.svg'
+import error_outline from './ic_error_outline_48px.svg'
+import warning from './ic_warning_48px.svg'
+
+const icons = {
+    add_alert,
+    error,
+    error_outline,
+    warning
+}
+
+export default {
+    icons,
+    names: Object.keys(icons)
+}
