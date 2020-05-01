8e43b7c24ccd884c624f9696e919039f1a491ca0
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-05-01 22:18:18 +0400

    add hardware icons

---
+++ src/icons/hardware/index.js
@@ -0,0 +1,72 @@
+import keyboard_arrow_down from './ic_keyboard_arrow_down_48px.svg'
+import keyboard from './ic_keyboard_48px.svg'
+import keyboard_return from './ic_keyboard_return_48px.svg'
+import laptop from './ic_laptop_48px.svg'
+import laptop_mac from './ic_laptop_mac_48px.svg'
+import keyboard_arrow_up from './ic_keyboard_arrow_up_48px.svg'
+import phone_android from './ic_phone_android_48px.svg'
+import keyboard_voice from './ic_keyboard_voice_48px.svg'
+import headset_mic from './ic_headset_mic_48px.svg'
+import keyboard_capslock from './ic_keyboard_capslock_48px.svg'
+import keyboard_hide from './ic_keyboard_hide_48px.svg'
+import keyboard_backspace from './ic_keyboard_backspace_48px.svg'
+import keyboard_arrow_left from './ic_keyboard_arrow_left_48px.svg'
+import keyboard_arrow_right from './ic_keyboard_arrow_right_48px.svg'
+import keyboard_tab from './ic_keyboard_tab_48px.svg'
+import laptop_windows from './ic_laptop_windows_48px.svg'
+import phonelink from './ic_phonelink_48px.svg'
+import memory from './ic_memory_48px.svg'
+import phone_iphone from './ic_phone_iphone_48px.svg'
+import laptop_chromebook from './ic_laptop_chromebook_48px.svg'
+import mouse from './ic_mouse_48px.svg'
+import headset from './ic_headset_48px.svg'
+import gamepad from './ic_gamepad_48px.svg'
+import dock from './ic_dock_48px.svg'
+import devices_other from './ic_devices_other_48px.svg'
+import device_hub from './ic_device_hub_24px.svg'
+import developer_board from './ic_developer_board_48px.svg'
+import desktop_windows from './ic_desktop_windows_48px.svg'
+import desktop_mac from './ic_desktop_mac_48px.svg'
+import computer from './ic_computer_48px.svg'
+import cast_connected from './ic_cast_connected_48px.svg'
+import cast from './ic_cast_48px.svg'
+
+const icons = {
+    keyboard_arrow_down,
+    keyboard,
+    keyboard_return,
+    laptop,
+    laptop_mac,
+    keyboard_arrow_up,
+    phone_android,
+    keyboard_voice,
+    headset_mic,
+    keyboard_capslock,
+    keyboard_hide,
+    keyboard_backspace,
+    keyboard_arrow_left,
+    keyboard_arrow_right,
+    keyboard_tab,
+    laptop_windows,
+    phonelink,
+    memory,
+    phone_iphone,
+    laptop_chromebook,
+    mouse,
+    headset,
+    gamepad,
+    dock,
+    devices_other,
+    device_hub,
+    developer_board,
+    desktop_windows,
+    desktop_mac,
+    computer,
+    cast_connected,
+    cast
+}
+
+export default {
+    icons,
+    names: Object.keys(icons)
+}
