35c145a4f409ff72cbc39a932014caaddf99d9dc
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-05-01 22:17:43 +0400

    add device icons

---
+++ src/icons/device/index.js
@@ -0,0 +1,186 @@
+import signal_cellular_off from './ic_signal_cellular_off_48px.svg'
+import brightness_auto from './ic_brightness_auto_48px.svg'
+import signal_wifi_statusbar_3_bar from './ic_signal_wifi_statusbar_3_bar_26x24px.svg'
+import signal_cellular_connected_no_internet_4_bar from './ic_signal_cellular_connected_no_internet_4_bar_48px.svg'
+import wallpaper from './ic_wallpaper_48px.svg'
+import brightness_medium from './ic_brightness_medium_48px.svg'
+import signal_cellular_no_sim from './ic_signal_cellular_no_sim_48px.svg'
+import signal_wifi_1_bar from './ic_signal_wifi_1_bar_48px.svg'
+import data_usage from './ic_data_usage_48px.svg'
+import bluetooth_disabled from './ic_bluetooth_disabled_48px.svg'
+import screen_lock_rotation from './ic_screen_lock_rotation_48px.svg'
+import network_wifi from './ic_network_wifi_48px.svg'
+import screen_rotation from './ic_screen_rotation_48px.svg'
+import signal_wifi_3_bar_lock from './ic_signal_wifi_3_bar_lock_48px.svg'
+import signal_wifi_1_bar_lock from './ic_signal_wifi_1_bar_lock_48px.svg'
+import signal_cellular_connected_no_internet_3_bar from './ic_signal_cellular_connected_no_internet_3_bar_48px.svg'
+import devices from './ic_devices_48px.svg'
+import brightness_high from './ic_brightness_high_48px.svg'
+import battery_unknown from './ic_battery_unknown_48px.svg'
+import signal_wifi_2_bar_lock from './ic_signal_wifi_2_bar_lock_48px.svg'
+import signal_wifi_statusbar_connected_no_internet_2 from './ic_signal_wifi_statusbar_connected_no_internet_2_26x24px.svg'
+import signal_wifi_statusbar_2_bar from './ic_signal_wifi_statusbar_2_bar_26x24px.svg'
+import signal_cellular_4_bar from './ic_signal_cellular_4_bar_48px.svg'
+import location_disabled from './ic_location_disabled_48px.svg'
+import sd_storage from './ic_sd_storage_48px.svg'
+import gps_fixed from './ic_gps_fixed_48px.svg'
+import signal_cellular_connected_no_internet_1_bar from './ic_signal_cellular_connected_no_internet_1_bar_48px.svg'
+import screen_lock_portrait from './ic_screen_lock_portrait_48px.svg'
+import settings_system_daydream from './ic_settings_system_daydream_48px.svg'
+import signal_cellular_connected_no_internet_2_bar from './ic_signal_cellular_connected_no_internet_2_bar_48px.svg'
+import signal_wifi_statusbar_1_bar from './ic_signal_wifi_statusbar_1_bar_26x24px.svg'
+import signal_wifi_statusbar_4_bar from './ic_signal_wifi_statusbar_4_bar_26x24px.svg'
+import signal_wifi_statusbar_connected_no_internet_4 from './ic_signal_wifi_statusbar_connected_no_internet_4_26x24px.svg'
+import bluetooth from './ic_bluetooth_48px.svg'
+import signal_wifi_0_bar from './ic_signal_wifi_0_bar_48px.svg'
+import wifi_lock from './ic_wifi_lock_48px.svg'
+import screen_lock_landscape from './ic_screen_lock_landscape_48px.svg'
+import network_cell from './ic_network_cell_48px.svg'
+import signal_cellular_3_bar from './ic_signal_cellular_3_bar_48px.svg'
+import brightness_low from './ic_brightness_low_48px.svg'
+import signal_cellular_0_bar from './ic_signal_cellular_0_bar_48px.svg'
+import usb from './ic_usb_48px.svg'
+import signal_wifi_2_bar from './ic_signal_wifi_2_bar_48px.svg'
+import signal_wifi_4_bar_lock from './ic_signal_wifi_4_bar_lock_48px.svg'
+import signal_cellular_1_bar from './ic_signal_cellular_1_bar_48px.svg'
+import gps_off from './ic_gps_off_48px.svg'
+import dvr from './ic_dvr_48px.svg'
+import signal_wifi_statusbar_not_connected from './ic_signal_wifi_statusbar_not_connected_26x24px.svg'
+import signal_wifi_off from './ic_signal_wifi_off_48px.svg'
+import gps_not_fixed from './ic_gps_not_fixed_48px.svg'
+import signal_wifi_4_bar from './ic_signal_wifi_4_bar_48px.svg'
+import signal_wifi_statusbar_connected_no_internet_1 from './ic_signal_wifi_statusbar_connected_no_internet_1_26x24px.svg'
+import wifi_tethering from './ic_wifi_tethering_48px.svg'
+import signal_cellular_connected_no_internet_0_bar from './ic_signal_cellular_connected_no_internet_0_bar_48px.svg'
+import developer_mode from './ic_developer_mode_48px.svg'
+import signal_cellular_2_bar from './ic_signal_cellular_2_bar_48px.svg'
+import storage from './ic_storage_48px.svg'
+import location_searching from './ic_location_searching_48px.svg'
+import signal_wifi_statusbar_null from './ic_signal_wifi_statusbar_null_26x24px.svg'
+import signal_cellular_null from './ic_signal_cellular_null_48px.svg'
+import graphic_eq from './ic_graphic_eq_48px.svg'
+import widgets from './ic_widgets_48px.svg'
+import bluetooth_connected from './ic_bluetooth_connected_48px.svg'
+import signal_wifi_statusbar_connected_no_internet_3 from './ic_signal_wifi_statusbar_connected_no_internet_3_26x24px.svg'
+import signal_wifi_3_bar from './ic_signal_wifi_3_bar_48px.svg'
+import bluetooth_searching from './ic_bluetooth_searching_48px.svg'
+import signal_wifi_statusbar_connected_no_internet from './ic_signal_wifi_statusbar_connected_no_internet_26x24px.svg'
+import battery_full from './ic_battery_full_48px.svg'
+import battery_std from './ic_battery_std_48px.svg'
+import battery_charging_full from './ic_battery_charging_full_48px.svg'
+import battery_charging_90 from './ic_battery_charging_90_48px.svg'
+import battery_charging_80 from './ic_battery_charging_80_48px.svg'
+import battery_charging_60 from './ic_battery_charging_60_48px.svg'
+import battery_charging_50 from './ic_battery_charging_50_48px.svg'
+import battery_charging_30 from './ic_battery_charging_30_48px.svg'
+import battery_charging_20 from './ic_battery_charging_20_48px.svg'
+import battery_alert from './ic_battery_alert_48px.svg'
+import battery_90 from './ic_battery_90_48px.svg'
+import battery_80 from './ic_battery_80_48px.svg'
+import battery_60 from './ic_battery_60_48px.svg'
+import battery_50 from './ic_battery_50_48px.svg'
+import battery_30 from './ic_battery_30_48px.svg'
+import battery_20 from './ic_battery_20_48px.svg'
+import airplanemode_inactive from './ic_airplanemode_inactive_48px.svg'
+import airplanemode_active from './ic_airplanemode_active_48px.svg'
+import add_alarm from './ic_add_alarm_48px.svg'
+import access_time from './ic_access_time_48px.svg'
+import access_alarms from './ic_access_alarms_48px.svg'
+import access_alarm from './ic_access_alarm_48px.svg'
+
+const icons = {
+    signal_cellular_off,
+    brightness_auto,
+    signal_wifi_statusbar_3_bar,
+    signal_cellular_connected_no_internet_4_bar,
+    wallpaper,
+    brightness_medium,
+    signal_cellular_no_sim,
+    signal_wifi_1_bar,
+    data_usage,
+    bluetooth_disabled,
+    screen_lock_rotation,
+    network_wifi,
+    screen_rotation,
+    signal_wifi_3_bar_lock,
+    signal_wifi_1_bar_lock,
+    signal_cellular_connected_no_internet_3_bar,
+    devices,
+    brightness_high,
+    battery_unknown,
+    signal_wifi_2_bar_lock,
+    signal_wifi_statusbar_connected_no_internet_2,
+    signal_wifi_statusbar_2_bar,
+    signal_cellular_4_bar,
+    location_disabled,
+    sd_storage,
+    gps_fixed,
+    signal_cellular_connected_no_internet_1_bar,
+    screen_lock_portrait,
+    settings_system_daydream,
+    signal_cellular_connected_no_internet_2_bar,
+    signal_wifi_statusbar_1_bar,
+    signal_wifi_statusbar_4_bar,
+    signal_wifi_statusbar_connected_no_internet_4,
+    bluetooth,
+    signal_wifi_0_bar,
+    wifi_lock,
+    screen_lock_landscape,
+    network_cell,
+    signal_cellular_3_bar,
+    brightness_low,
+    signal_cellular_0_bar,
+    usb,
+    signal_wifi_2_bar,
+    signal_wifi_4_bar_lock,
+    signal_cellular_1_bar,
+    gps_off,
+    dvr,
+    signal_wifi_statusbar_not_connected,
+    signal_wifi_off,
+    gps_not_fixed,
+    signal_wifi_4_bar,
+    signal_wifi_statusbar_connected_no_internet_1,
+    wifi_tethering,
+    signal_cellular_connected_no_internet_0_bar,
+    developer_mode,
+    signal_cellular_2_bar,
+    storage,
+    location_searching,
+    signal_wifi_statusbar_null,
+    signal_cellular_null,
+    graphic_eq,
+    widgets,
+    bluetooth_connected,
+    signal_wifi_statusbar_connected_no_internet_3,
+    signal_wifi_3_bar,
+    bluetooth_searching,
+    signal_wifi_statusbar_connected_no_internet,
+    battery_full,
+    battery_std,
+    battery_charging_full,
+    battery_charging_90,
+    battery_charging_80,
+    battery_charging_60,
+    battery_charging_50,
+    battery_charging_30,
+    battery_charging_20,
+    battery_alert,
+    battery_90,
+    battery_80,
+    battery_60,
+    battery_50,
+    battery_30,
+    battery_20,
+    airplanemode_inactive,
+    airplanemode_active,
+    add_alarm,
+    access_time,
+    access_alarms,
+    access_alarm
+}
+
+export default {
+    icons,
+    names: Object.keys(icons)
+}
